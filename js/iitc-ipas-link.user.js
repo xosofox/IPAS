a2cf89d0bac9cd00c3113de2318c4971981faef8
Peter Dietrich <peter.dietrich@smartpixelmedia.de>
2013-04-08 09:41:05 +0200

    Fixes a bug with <8 resos on userscript, thanks eggers

--- js/iitc-ipas-link.user.js
+++ js/iitc-ipas-link.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @id             iitc-plugin-ipas-link@graphracer
 // @name           IITC Plugin: simulate an attack on portal
-// @version        0.2.0.20130404.140000
+// @version        0.2.1.20130408.100000
 // @namespace      https://github.com/xosofox/IPAS
 // @updateURL      http://ipas.graphracer.com/js/iitc-ipas-link.meta.js
 // @downloadURL    http://ipas.graphracer.com/js/iitc-ipas-link.user.js
@@ -14,48 +14,53 @@
 
 function wrapper() {
 // ensure plugin framework is there, even if iitc is not yet loaded
-if(typeof window.plugin !== 'function') window.plugin = function() {};
+    if (typeof window.plugin !== 'function') window.plugin = function () {
+    };
 
 // PLUGIN START ////////////////////////////////////////////////////////
 
 // use own namespace for plugin
-window.plugin.ipasLink = function() {};
+    window.plugin.ipasLink = function () {
+    };
 
-window.plugin.ipasLink.setupCallback = function() {
-      addHook('portalDetailsUpdated', window.plugin.ipasLink.addLink);
-}
+    window.plugin.ipasLink.setupCallback = function () {
+        addHook('portalDetailsUpdated', window.plugin.ipasLink.addLink);
+    }
 
-window.plugin.ipasLink.addLink = function(d) {
-    $('.linkdetails').append('<aside style="text-align: center; display: block"><a href="http://ipas.graphracer.com/index.html#' + window.plugin.ipasLink.getHash(d.portalDetails) + '" target="ipaswindow">simulate attack with IPAS</a></aside>');
-}
+    window.plugin.ipasLink.addLink = function (d) {
+        $('.linkdetails').append('<aside style="text-align: center; display: block"><a href="http://ipas.graphracer.com/index.html#' + window.plugin.ipasLink.getHash(d.portalDetails) + '" target="ipaswindow">simulate attack with IPAS</a></aside>');
+    }
 
-window.plugin.ipasLink.getHash = function(d) {
-    console.log(d);
-    var hashParts=[];
-    $.each(d.resonatorArray.resonators, function(ind, reso) {
-        hashParts.push(reso.level + "," + reso.distanceToPortal + "," + reso.energyTotal);
-    });
-    console.log(d);
-    return hashParts.join(";")+"|" + "r,c,0,v";
-}
 
-var setup =  function() {
-  window.plugin.ipasLink.setupCallback();
-}
+    window.plugin.ipasLink.getHash = function (d) {
+        var hashParts = [];
+        $.each(d.resonatorArray.resonators, function (ind, reso) {
+            if (reso) {
+                hashParts.push(reso.level + "," + reso.distanceToPortal + "," + reso.energyTotal);
+            } else {
+                hashParts.push("1,20,0");
+            }
+        });
+        return hashParts.join(";") + "|" + "r,c,0,v";
+    }
+
+    var setup = function () {
+        window.plugin.ipasLink.setupCallback();
+    }
 
 // PLUGIN END //////////////////////////////////////////////////////////
 
-if(window.iitcLoaded && typeof setup === 'function') {
-  setup();
-} else {
-  if(window.bootPlugins)
-    window.bootPlugins.push(setup);
-  else
-    window.bootPlugins = [setup];
-}
+    if (window.iitcLoaded && typeof setup === 'function') {
+        setup();
+    } else {
+        if (window.bootPlugins)
+            window.bootPlugins.push(setup);
+        else
+            window.bootPlugins = [setup];
+    }
 } // wrapper end
 // inject code into site context
 var script = document.createElement('script');
-script.appendChild(document.createTextNode('('+ wrapper +')();'));
+script.appendChild(document.createTextNode('(' + wrapper + ')();'));
 (document.body || document.head || document.documentElement).appendChild(script);
 
