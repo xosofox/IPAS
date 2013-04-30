5a70dcae6af2ba23bc1fef2b04e5a5ca7a3d3b86
Peter Dietrich <peter.dietrich@smartpixelmedia.de>
2013-04-30 10:43:33 +0200

    minifiy

---
+++ js/compress.bat
@@ -0,0 +1,3 @@
+type models\* >COMPRESS.js
+type views\* >>COMPRESS.js
+"c:\Program Files (x86)\Java\jre7\bin\java.exe" -jar "a:\PortableApps\yuicompressor\yuicompressor-2.4.7.jar" COMPRESS.js -o ipas.min.js
