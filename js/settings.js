22e32c75bcc05c7541f379e05c8d63330c821b45
Peter Dietrich <peter.dietrich@smartpixelmedia.de>
2013-05-29 00:19:45 +0200

    Keep a note on possible xmp cost change

--- js/settings.js
+++ js/settings.js
@@ -34,6 +34,7 @@ var burster_range = [42, 48, 58, 72, 90, 112, 138, 168];
 var burster_steps = [8, 9, 11, 14, 18, 22, 27, 33];
 var level_color = ['#000', "#fece5a", "#ffa630", "#ff7315", "#e40000", "#fd2992", "#eb26cd", "#c124e0", "#9627f4"];
 var burster_cost = [ 50, 100, 150, 200, 250, 300, 350, 400 ];
+//"xmpFiringCostByLevel": [ 10, 20, 70, 140, 250, 360, 490, 640 ],  http://www.reddit.com/r/Ingress/comments/1f7byk/spotted_heatsink_force_amplifier_portal_turret/
 var reso_deploy_cost = [ 50, 100, 150, 200, 250, 300, 350, 400 ];
 var shield_deploy_cost = {
     "-": 0,
