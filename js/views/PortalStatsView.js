7968c6d190e4d0e0f40d41ff316f0503f9abe677
Peter Dietrich <peter.dietrich@smartpixelmedia.de>
2013-06-23 21:37:41 +0200

    Introduce PortalStats
    only total mitigation so far

---
+++ js/views/PortalStatsView.js
@@ -0,0 +1,23 @@
+var PortalStatsView = Backbone.View.extend({
+    model: Portal,
+    initialize: function () {
+        this.listenTo(this.model, "change", this.render)
+        this.listenTo(this.model.mods, "add remove change", this.render)
+        this.listenTo(this.model.links, "add remove change", this.render)
+        this.template=_.template(PortalStatsTemplate);
+        this.$el.html(this.template());
+        this.render();
+    },
+    render: function () {
+        var totMit = this.model.totalMitigation();
+        var text= totMit;
+        if (totMit==95) {
+            text += " (max)";
+        }
+        $('#totalMitigation').text(text);
+    }
+});
+
+var PortalStatsTemplate = '\
+    Total mitigation: <span id="totalMitigation">0</span><br />\
+';
